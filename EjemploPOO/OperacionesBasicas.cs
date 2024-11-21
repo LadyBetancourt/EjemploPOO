f3679697cedc00ede484b2b5597c17a8d16bc80c
LadyBetancourt <lady_betancourt24@unikino.edu.mx>
2024-11-21 07:53:10 -0700

    Agregar archivos de proyecto.

---
+++ EjemploPOO/OperacionesBasicas.cs
@@ -0,0 +1,25 @@
+﻿namespace EjemploPOO
+{
+    internal class OperacionesBasicas
+    {
+        public int Sumar(int primerNúmero, int segundoNúmero) 
+        {
+            return primerNúmero + segundoNúmero;
+        }
+
+        public int Restar(int primerNúmero, int segundoNúmero)
+        {
+            return primerNúmero - segundoNúmero;
+        }
+
+        public int Multiplicar(int primerNúmero, int segundoNúmero) 
+        {
+            return primerNúmero * segundoNúmero;
+        }
+
+        public decimal Division(int primerNúmero, int segundoNúmero) 
+        {
+            return primerNúmero / segundoNúmero;
+        }
+    }
+}
